ff00865cc9482844e226eac49ac91cc36ec4bddf
Nicolas <nicobou60@gmail.com>
2025-12-18 10:36:59 +0100

    transaction historique

--- backend-bank/src/main/java/com/bank/banksystem/controller/TransactionController.java
+++ backend-bank/src/main/java/com/bank/banksystem/controller/TransactionController.java
@@ -2,12 +2,15 @@ package com.bank.banksystem.controller;
 
 import com.bank.banksystem.dto.transaction.CreateTransactionDTO;
 import com.bank.banksystem.dto.transaction.TransactionDTO;
+import com.bank.banksystem.model.Transaction;
 import com.bank.banksystem.service.interfaces.ITransactionService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -25,4 +28,20 @@ public class TransactionController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/account/{accountId}")
+    public ResponseEntity<?> getTransactionsByAccount(
+            @PathVariable Long accountId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String type) {
+        try {
+            List<TransactionDTO> transactions = transactionService.getTransactionsByAccount(accountId, page, size, type);
+
+            return ResponseEntity.ok(transactions);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
--- backend-bank/src/main/java/com/bank/banksystem/service/interfaces/ITransactionService.java
+++ backend-bank/src/main/java/com/bank/banksystem/service/interfaces/ITransactionService.java
@@ -3,6 +3,9 @@ package com.bank.banksystem.service.interfaces;
 import com.bank.banksystem.dto.transaction.CreateTransactionDTO;
 import com.bank.banksystem.dto.transaction.TransactionDTO;
 
+import java.util.List;
+
 public interface ITransactionService {
     TransactionDTO createTransaction(CreateTransactionDTO createTransactionDTO);
+    List<TransactionDTO> getTransactionsByAccount(Long accountId, int page, int size, String type);
 }
